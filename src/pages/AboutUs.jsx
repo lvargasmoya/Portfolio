c3ea4bc78938e11a0c35186b993f31b1a680c31b
lvargasmoya <32650033+lvargasmoya@users.noreply.github.com>
2023-01-15 11:34:07 -0600

    ServicesSection

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import AboutSections from "../components/AboutSections";
+import ServicesSection from "../components/ServicesSection";
 //Page Components
 
 const AboutUs = () => {
-  return <AboutSections />;
+  return (
+    <>
+      <AboutSections />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
